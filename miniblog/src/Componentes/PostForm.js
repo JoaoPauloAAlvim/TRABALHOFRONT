5201f872b8a3656eefb6692c3ae140d412f5ab0c
JoaoPauloAAlvim <alvesalvimj@gmail.com>
2024-09-14 16:53:40 -0300

    Correção do componente de formulario

--- miniblog/src/Componentes/PostForm.js
+++ miniblog/src/Componentes/PostForm.js
@@ -1,57 +1,159 @@
+// import React from "react";
+// import styled from "styled-components";
+
+// const Formulario = styled.div`  // Alterei para um seletor válido
+//   background-color: red;
+//   padding: 20px;
+//   display: flex;
+//   flex-direction: column;
+//   gap: 10px;
+// `;
+
+// export default class PostForm extends React.Component {
+//   state = {
+//     titulo: "",
+//     descricao: "",
+//     imagem: "",
+//     posts: []
+//   };
+
+//   onChangeTitulo = (event) => {
+//     this.setState({ titulo: event.target.value });
+//   };
+
+//   onChangeDescricao = (event) => {
+//     this.setState({ descricao: event.target.value });
+//   };
+
+//   onChangeImagem = (event) => {
+//     this.setState({ imagem: event.target.value });
+//   };
+
+//   adicionarPost = () => {
+//     let titulo= this.state.titulo;
+//     let descricao = this.state.descricao;
+//     let imagem = this.state.imagem || "https://copilot.microsoft.com/images/create/quando-alguem-nao-colocar-uma-imagem-no-meu-imput-/1-66e5d40db6e6463f929973236a4286d6?id=%2fzPoBoa%2bJQ%2fwqmQXX%2fmK1w%3d%3d&view=detailv2&idpp=genimg&idpclose=1&thId=OIG4.De0JXDhVWzlHUqaVkVTU&skey=H7srwskiLk25M2JpVeLA5vJ6DXO_8p7LrIMt3YhhZP4&FORM=SYDBIC"; 
+//     let id = Math.random();
+//     const novoPost = [...this.state.posts,{ titulo: titulo, descricao: descricao,imagem:imagem, id: id },]
+//     this.setState({posts:novoPost});
+//     this.setState({
+//       titulo:"",
+//       descricao:"",
+//       imagem:"",
+//     });
+//   };
+    
+
+    
+//   };
+
+//   render() {
+//     return (
+//       <Formulario>
+//         <input
+//           type="text"
+//           placeholder="Digite o título da sua postagem (máximo 50 caracteres)"
+//           onChange={this.onChangeTitulo}
+//           value={this.state.titulo}
+//           name="titulo"
+//         />
+//         <input
+//           type="text"
+//           placeholder="Digite a descrição do seu post"
+//           onChange={this.onChangeDescricao}
+//           value={this.state.descricao}
+//           name="descricao"
+//         />
+//         <input
+//           type="text"
+//           placeholder="Digite a URL da sua imagem (opcional)"
+//           onChange={this.onChangeImagem}
+//           value={this.state.imagem}
+//           name="imagem"
+//         />
+//         <button onClick={this.adicionarPost}>Postar</button>
+//       </Formulario>
+//     );
+// };
+
+
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
+
+const Formulario = styled.div`
+  background-color: red;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 export default class PostForm extends React.Component {
-  onChangeTitulo = (event)=>{
-    this.setState({ titulo: event.target.value })
-  }
-  onChangeDescricao = (event)=>{
-    this.setState({ descricao: event.target.value })
-  }
-  onChangeImagem = (event)=>{
-    this.setState({ imagem: event.target.value })
-  }
+  state = {
+    titulo: "",
+    descricao: "",
+    imagem: "",
+    posts: []
+  };
 
-  adicionarPost=()=>{
-    let titulo= this.state.titulo;
-    let descricao=this.state.descricao;
-    let imagem= this.state.imagem || "https://copilot.microsoft.com/images/create/quando-alguem-nao-colocar-uma-imagem-no-meu-imput-/1-66e5d40db6e6463f929973236a4286d6?id=%2fzPoBoa%2bJQ%2fwqmQXX%2fmK1w%3d%3d&view=detailv2&idpp=genimg&idpclose=1&thId=OIG4.De0JXDhVWzlHUqaVkVTU&skey=H7srwskiLk25M2JpVeLA5vJ6DXO_8p7LrIMt3YhhZP4&FORM=SYDBIC";
-    let id=Math.random();
+  onChangeTitulo = (event) => {
+    this.setState({ titulo: event.target.value });
+  };
 
-    let posts =[...this.state.posts,
-        { titulo: titulo, descricao: descricao, imagem: imagem,id:id },
+  onChangeDescricao = (event) => {
+    this.setState({ descricao: event.target.value });
+  };
+
+  onChangeImagem = (event) => {
+    this.setState({ imagem: event.target.value });
+  };
+
+  adicionarPost = () => {
+    let titulo = this.state.titulo;
+    let descricao = this.state.descricao;
+    let imagem = this.state.imagem || "https://copilot.microsoft.com/images/create/quando-alguem-nao-colocar-uma-imagem-no-meu-imput-/1-66e5d40db6e6463f929973236a4286d6?id=%2fzPoBoa%2bJQ%2fwqmQXX%2fmK1w%3d%3d&view=detailv2&idpp=genimg&idpclose=1&thId=OIG4.De0JXDhVWzlHUqaVkVTU&skey=H7srwskiLk25M2JpVeLA5vJ6DXO_8p7LrIMt3YhhZP4&FORM=SYDBIC"; 
+    let id = Math.random();
+
+    // Correção: Agora o estado de posts está sendo atualizado corretamente.
+    const novoPost = [
+      ...this.state.posts,
+      { titulo: titulo, descricao: descricao, imagem: imagem, id: id }
     ];
-    this.setState({posts:posts});
+
     this.setState({
-        titulo: "",
-        descricao: "",
-        imagem:"",
-      });
-  }
+      posts: novoPost,
+      titulo: "",
+      descricao: "",
+      imagem: "",
+    });
+  };
 
   render() {
     return (
-      <div>
-        <input 
-            type="text"
-            placeholder="Digite o título da sua postagem (maximo 50 caracteres)" 
-            onChange={this.onChangeTítulo}
-            value={this.state.titulo}
-            name="título"/>
-        <input 
-            type="text"
-            placeholder="Digite a descrição do seu post" 
-            onChange={this.onChangeDescricao}
-            value={this.state.descricao}
-            name="descrição"/>
-        <input 
-            type="text"
-            placeholder="Digite a url da sua imagem (opcional)" 
-            onChange={this.onChangeImagem}
-            value={this.state.imagem}
-            name="imagem"/>
+      <Formulario>
+        <input
+          type="text"
+          placeholder="Digite o título da sua postagem (máximo 50 caracteres)"
+          onChange={this.onChangeTitulo}
+          value={this.state.titulo}
+          name="titulo"
+        />
+        <input
+          type="text"
+          placeholder="Digite a descrição do seu post"
+          onChange={this.onChangeDescricao}
+          value={this.state.descricao}
+          name="descricao"
+        />
+        <input
+          type="text"
+          placeholder="Digite a URL da sua imagem (opcional)"
+          onChange={this.onChangeImagem}
+          value={this.state.imagem}
+          name="imagem"
+        />
         <button onClick={this.adicionarPost}>Postar</button>
-      </div>
+      </Formulario>
     );
   }
 }
--- miniblog/src/Componentes/PostItem.js
+++ miniblog/src/Componentes/PostItem.js
@@ -1,38 +1,48 @@
+
 import React from "react";
 import styled from "styled-components";
 
-export default class PostItem extends React.Component{
-    state = {
-        posts:[
-            {titulo:"",descricao:"",imagem:"",id:Math.random()}
-        ],
-        titulo:"",
-        descricao:"",
-        imagem:"",
-      };
-      removerPost = (id) => {
-        const novaListaDePosts = [...this.state.posts];
-        const postagens = copiaDaLista.filter((post) => {
-          return id !== post.id;
-        });
-        this.setState({ posts: postagens });
-      };
-    render(){
-        let listagem = this.state.posts.map((post) => {
-        return<>
-            <div key={post.id}>
-                <p titulo={post.titulo}></p>
-                <p descricao={post.descricao}></p>
-                <img imagem={post.imagem}/>
-                <button
-                    onClick={() => {
-                        this.removerPost(post.id);
-                    }}
-                >
-                Deletar
-                </button>
-            </div>
-        </>
-        });
-    }
+const PostBlog = styled.div`
+  background-color: red;
+  padding: 10px;
+  margin-bottom: 10px;
+`;
+
+export default class PostItem extends React.Component {
+  state = {
+    posts: [
+      { titulo: "Primeiro Post", descricao: "Descrição do post", imagem: "url_da_imagem", id: Math.random() },
+    ],
+    titulo: "",
+    descricao: "",
+    imagem: "",
+  };
+
+  removerPost = (id) => {
+    const novaListaDePosts = this.state.posts.filter((post) => post.id !== id);
+    this.setState({ posts: novaListaDePosts });
+  };
+
+  render() {
+    let listagem = this.state.posts.map((post) => {
+      return (
+        <PostBlog key={post.id}>
+          <div>
+            <p>{post.titulo}</p>
+            <p>{post.descricao}</p>
+            <img src={post.imagem} alt={post.descricao} />
+            <button
+              onClick={() => {
+                this.removerPost(post.id);
+              }}
+            >
+              Deletar
+            </button>
+          </div>
+        </PostBlog>
+      );
+    });
+
+    return <div>{listagem}</div>;
+  }
 }
